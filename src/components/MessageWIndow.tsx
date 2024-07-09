d7207a7e8a21c396577d994b60b18adb51c1f1b1
Takodakko <monchichi@Monchichis-Air.lan>
2024-07-09 19:31:27 -0400

    added more icons

--- src/components/MessageWIndow.tsx
+++ src/components/MessageWIndow.tsx
@@ -1,14 +1,19 @@
 import './MessageWindow.css';
 import { queryMessageType } from '../App';
+import imageList from '../imageList';
 
 function MessageWindow(props: {content: queryMessageType, messageWindowClose: Function}) {
   const { content, messageWindowClose } = props;
+  
   const eventType = content[0];
   const isQuery = eventType === 'query';
   const points = content[1];
   const text = content[2];
 
   const buttonText = isQuery ? "Yeah, let's go for it!" : 'Ok';
+
+  const image = isQuery ? '' : imageList[eventType];
+  const imageTag = <img src={image}></img>
 
   return (
     <>
@@ -17,6 +22,7 @@ function MessageWindow(props: {content: queryMessageType, messageWindowClose: Fu
           {text}<br></br> 
           {points !== 0 ? `Points: ${points}` : ''}
         </div>
+        <div style={{height: '1em', width: '1em'}} >{imageTag}</div>
         <div className="button-container">
         <button onClick={() => messageWindowClose(true)} style={{display: isQuery ? 'block' : 'none'}} autoFocus={true}>
             No, I'd better not...
--- src/imageList.ts
+++ src/imageList.ts
@@ -1,12 +1,22 @@
 import sail from './assets/sail.jpg';
 import cargo from './assets/cargo.jpg';
 import waves from './assets/waves.avif';
+import chest from './assets/chest.png';
+import pit from './assets/pit.png';
+import snake from './assets/snake.png';
+import fruit from './assets/fruit.png';
+import nothing from './assets/nothing.png';
 
 /** List of image urls used in app */
 const imageList: Record<string, string> = {
     sail: sail,
     cargo: cargo,
     waves: waves,
+    fruit: fruit,
+    chest: chest,
+    snake: snake,
+    pit: pit,
+    nothing: nothing,
 };
 
 export default imageList
